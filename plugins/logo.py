a8188b35b8aa96e0345447ba371550536a5adf4b
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-09-29 21:17:40 +0530

    Add files via upload

--- plugins/logo.py
+++ plugins/logo.py
@@ -1,163 +1,104 @@
-from Stylish-Text-Bot/plugins/events.py import register
-import OWNER_ID
-t import telethn as tbot
-import os 
-from PIL import Image, ImageDraw, ImageFont
-import shutil 
-import random, re
+# Ultroid - UserBot
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+"""
+✘ Commands Available -
+
+• `{i}logo <text>`
+   Generate a logo of the given Text
+   Or Reply To image , to write ur text on it.
+   Or Reply To Font File, To write with that font.
+
+"""
 import glob
-import time
+import os
+import random
+
+from PIL import Image, ImageDraw, ImageFont
 from telethon.tl.types import InputMessagesFilterPhotos
 
+from . import *
 
 
+@ultroid_cmd(pattern="logo ?(.*)")
+async def logo_gen(event):
+    xx = await eor(event, get_string("com_1"))
+    name = event.pattern_match.group(1)
+    if not name:
+        await eor(xx, "`Give a name too!`", time=5)
+    bg_, font_ = None, None
+    if event.reply_to_msg_id:
+        temp = await event.get_reply_message()
+        if temp.media:
+            if hasattr(temp.media, "document"):
+                if "font" in temp.file.mime_type:
+                    font_ = await temp.download_media()
+                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
+                    font_ = await temp.download_media()
+            elif "pic" in mediainfo(temp.media):
+                bg_ = await temp.download_media()
+    if not bg_:
+        if event.client._bot:
+            SRCH = ["blur", "background", "neon lights", "wallpaper"]
+            res = autopicsearch(random.choice(SRCH))
+            res = "https://unsplash.com" + random.choice(res)["href"]
+            bst = bs(requests.get(res).content, "html.parser", from_encoding="utf-8")
+            ft = bst.find_all("img", "oCCRx")[0]["src"]
+            bg_ = await download_file(ft, "resources/downloads/logo.png")
+        else:
+            pics = []
+            async for i in event.client.iter_messages(
+                "@UltroidLogos", filter=InputMessagesFilterPhotos
+            ):
+                pics.append(i)
+            id_ = random.choice(pics)
+            bg_ = await id_.download_media()
 
-TELEGRAPH_MEDIA_LINKS = ["./DewmiBot/resources/download (1).jfif", 
-                         "./DewmiBot/resources/download (1).png", 
-                         "./DewmiBot/resources/download (2).jfif",
-                         "./DewmiBot/resources/download (2).png",
-                         "./DewmiBot/resources/download (3).jfif",    
-                         "./DewmiBot/resources/download (4).jfif", 
-                         "./DewmiBot/resources/download (5).jfif",
-                         "./DewmiBot/resources/download (7).jfif",
-                         "./DewmiBot/resources/download.jfif",
-                         "./DewmiBot/resources/download.png", 
-                         "./DewmiBot/resources/images (1).jfif",
-                         "./DewmiBot/resources/images (2).jfif",
-                         "./DewmiBot/resources/images (3).jfif",
-                         "./DewmiBot/resources/images (4).jfif", 
-                         "./DewmiBot/resources/images (5).jfif",
-                         "./DewmiBot/resources/images (6).jfif",
-                         "./DewmiBot/resources/images (7).jfif",     
-                         "./DewmiBot/resources/images (8).jfif", 
-                         "./DewmiBot/resources/images (9).jfif",
-                         "./DewmiBot/resources/images.jfif",
-                         "./DewmiBot/resources/images.png"
-                         ]
-
-@register(pattern="^/logo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
+    if not font_:
+        fpath_ = glob.glob("resources/fonts/*")
+        font_ = random.choice(fpath_)
+    if len(name) <= 8:
+        fnt_size = 150
+        strke = 10
+    elif len(name) >= 9:
+        fnt_size = 50
+        strke = 5
     else:
-       pass
- await event.reply('Wait..now ok!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open(random.choice(TELEGRAPH_MEDIA_LINKS))
+        fnt_size = 130
+        strke = 20
+    img = Image.open(bg_)
     draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 25
-    fillcolor = "gold"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./DewmiBot/resources/font.otf",40)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
+    font = ImageFont.truetype(font_, fnt_size)
+    w, h = draw.textsize(name, font=font)
+    h += int(h * 0.21)
     image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=2, stroke_fill="yellow")
-    fname2 = "Logo.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @szrosebot🇱🇰")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Join with us ~ @sl_bot_zone to use this, {e}')
-
-
-@register(pattern="^/wlogo ?(.*)")
-async def logo(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
-    else:
-       pass
- await event.reply('Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./DewmiBot/resources/photo_2021-08-21_23-14-49.png')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "white"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./DewmiBot/resources/Maghrib.ttf", 1000)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "Logo.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @szrosebot🇱🇰")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Join with us ~ @sl_bot_zone to use this, {e}')
-
-  
-
-@register(pattern="^/pandalogo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
-    else:
-       pass
- await event.reply('Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./DewmiBot/resources/pandabg.png')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 25
-    fillcolor = "gold"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./DewmiBot/resources/font.otf", 100)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "Logo.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @szrosebot🇱🇰")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Join with us ~ @sl_bot_zone to use this, {e}')
-  
-file_help = os.path.basename(__file__)
-file_help = file_help.replace(".py", "")
-file_helpo = file_help.replace("_", " ")
-
-
-__help__ = """
-@szrosebot🇱🇰
- ❍ /logo text :  Create your logo with your name
- ❍ /wlogo text :  Create your logo with your name
- ❍ /pandalogo :  Create your logo with your name
- """
-__mod_name__ = "Logo Maker"
+    draw.text(
+        ((image_width - w) / 2, (image_height - h) / 2),
+        name,
+        font=font,
+        fill=(255, 255, 255),
+    )
+    x = (image_width - w) / 2
+    y = (image_height - h) / 2
+    draw.text(
+        (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
+    )
+    flnme = "ultd.png"
+    img.save(flnme, "png")
+    await xx.edit("`Done!`")
+    if os.path.exists(flnme):
+        await event.client.send_file(
+            event.chat_id,
+            file=flnme,
+            caption=f"Logo by [{OWNER_NAME}](tg://user?id={OWNER_ID})",
+            force_document=True,
+        )
+        os.remove(flnme)
+        await xx.delete()
+    if os.path.exists(bg_):
+        os.remove(bg_)
+    if os.path.exists(font_) and not font_.startswith("resources/fonts"):
+        os.remove(font_)
